4527d069e229e835bc040174a0c9639acf5bda80
FrankMugagga <frank.mugagga@gmail.com>
2023-03-15 17:44:05 +0300

    Create sum file and tested it

---
+++ sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 = 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+
+
+/*
+const sum = require('./sum');
+
+test('adds 1+2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});*/
